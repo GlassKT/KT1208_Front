76cc15d079a808f434ddbb8bf3b03f17b052fe37
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-05-06 14:58:06 +0900

    leftbar (#17)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
+import Leftbar from "../common/leftbar/Leftbar.jsx";
 import wrapper from "../stores/configureStore";
 import { lightTheme } from "../styles/theme";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +13,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider theme={lightTheme}>
+        <Leftbar />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
